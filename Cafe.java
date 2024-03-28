f50d150051b00619fc0da0ca166122c4b83b157d
adk-saugat <saugat.adk12@gmail.com>
2024-03-28 13:03:03 -0500

    cafe commit

---
+++ Cafe.java
@@ -0,0 +1,132 @@
+// Name: Saugat Adhikari
+// Course: CSCI 2003 Spring 2024
+// Program Description: This program takes coffee order of the customer. And then calculates 
+//                      their total price for the coffee by giving discount if they have their 
+//                      membership, giving tip and adding taxes. It also shows then their total bill at last.
+
+import java.util.Scanner;
+
+public class Cafe {
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        // This is the header and the menu of the cafe.
+        System.out.println("\t\t\tCafe\n\t\t\t====\n");
+        System.out.println("\tMENU\n");
+        System.out.println("1. Cappuccino\t$4.99\n2. Espresso\t\t$2.99\n3. Latte\t\t\t$6.99\n");
+        System.out.println("Hello there! Welcome to the Cafe!!");
+
+        // This asks the customer if they want to check the price of the coffee and decide whether they want to buy or not.
+        System.out.print("\nWant to check your total price first? Type yes or no. -> ");
+        String checkChoice = userInput.nextLine();
+        while (!(checkChoice.toLowerCase().equals("yes") || checkChoice.toLowerCase().equals("no"))){
+            System.out.print("\nWrong! Just type Yes or No. ->");
+            checkChoice = userInput.nextLine();
+        }
+        // This runs if the customer want the price check and asks the coffee they want and the number, and then shows their would be cost.
+        if (checkChoice.toLowerCase().equals("yes")){
+            System.out.print("\nSo, which coffee you want to check the total price of? ->");
+            String coffeeCheck = userInput.nextLine();
+            while (!(coffeeCheck.equals("1") || coffeeCheck.equals("2") || coffeeCheck.equals("3") || coffeeCheck.toLowerCase().equals("cappuccino") || coffeeCheck.toLowerCase().equals("espresso") || coffeeCheck.toLowerCase().equals("latte"))){
+                System.out.print("Please choose coffee from the menu. -> ");
+                coffeeCheck = userInput.nextLine();
+            }
+            if (coffeeCheck.equals("1")){
+                coffeeCheck = "cappuccino";
+            }else if(coffeeCheck.equals("2")){
+                coffeeCheck = "espresso";
+            }else if(coffeeCheck.equals("3")){
+                coffeeCheck = "latte";
+            }
+            System.out.print("\nAnd, how many " + coffeeCheck + " do you want?->");
+            int numCheck = userInput.nextInt();
+            while (numCheck <= 0){
+                System.out.print("\nYou have to order one or more! So, how many?-> ");
+                numCheck = userInput.nextInt();
+            }
+            userInput.nextLine();
+            CafeCustomer checkInfo = new CafeCustomer("Customer", numCheck, false, coffeeCheck);
+            System.out.printf("\nYour total would be $%.2f.\n",checkInfo.calcTotal());
+
+        } 
+        // After checking, this will ask if the customer would like to order or not.
+        System.out.print("\nSo, would you like to order?-> ");
+        String orderCheck = userInput.nextLine();
+        while (!(orderCheck.toLowerCase().equals("yes") || orderCheck.toLowerCase().equals("no"))){
+            System.out.print("\nWrong! Just type Yes or No ->");
+            orderCheck = userInput.nextLine();
+        }
+        // If the customer don't want to order, this will terminate the program.
+        if (orderCheck.toLowerCase().equals("no")){
+            System.exit(0);    
+        }        
+
+        // This will ask the customer what they would like to order from the menu.
+        System.out.print("\nWhat would you like to order, then? -> ");
+
+        String coffeeChoice = userInput.nextLine();
+        while (!(coffeeChoice.equals("1") || coffeeChoice.equals("2") || coffeeChoice.equals("3") || coffeeChoice.toLowerCase().equals("cappuccino") || coffeeChoice.toLowerCase().equals("espresso") || coffeeChoice.toLowerCase().equals("latte"))){
+            System.out.print("We only serve coffee on the menu. Choose from the menu. -> ");
+            coffeeChoice = userInput.nextLine();
+        }
+
+        if (coffeeChoice.equals("1")){
+            coffeeChoice = "cappuccino";
+        }else if(coffeeChoice.equals("2")){
+            coffeeChoice = "espresso";
+        }else if(coffeeChoice.equals("3")){
+            coffeeChoice = "latte";
+        }
+        // It asks for the number of coffee they want.
+        System.out.print("\nHow many " + coffeeChoice + " do you need? -> ");
+        int numOfCoffee = userInput.nextInt();
+        while (numOfCoffee <= 0){
+            System.out.print("\nYou have to order one or more! So, how many?-> ");
+            numOfCoffee = userInput.nextInt();
+        }
+        userInput.nextLine();
+
+        // This asks for the name of the customer.
+        System.out.print("\nWhat is your name? -> ");
+        String customerName = userInput.nextLine();
+
+        // This part checks whether customer have membership of the cafe and gives discount if they have it.
+        System.out.print("\nDo you have membership of this cafe? Type Yes or No-> ");
+        String memberCheck = userInput.nextLine();
+
+        while (!(memberCheck.toLowerCase().equals("yes") || memberCheck.toLowerCase().equals("no"))){
+            System.out.print("\nWrong! Just type Yes or No ->");
+            memberCheck = userInput.nextLine();
+        }
+        boolean isMember = false;
+        if (memberCheck.toLowerCase().equals("yes")){
+            isMember = true;
+            System.out.println("You'll get 20% discount for your order!!");
+        } 
+        // Creating the customer1 object with the given parameters.
+        CafeCustomer customer1 = new CafeCustomer(customerName, numOfCoffee, isMember, coffeeChoice);
+        
+        // Shows the total price of the coffee with the discount(if any).
+        System.out.printf("\nYour total for %d %s is $%.2f.", numOfCoffee, coffeeChoice, customer1.calcTotal());
+
+        // Asks for a tip to the customer.
+        System.out.print("\n\nDo you want to give us a tip? If yes, type the amount, else put 0.-> ");
+        double tip = userInput.nextDouble();
+        while (tip < 0){
+            System.out.print("\nYou can't give negative tip. So, try again.-> ");
+            tip = userInput.nextDouble();
+        }
+        customer1.setTip(tip);
+
+        // Calculates the final total cost of the coffees.
+        double finalTotal = customer1.calcTotal() + tip;
+        System.out.printf("\n\nSo, your final total is $%.2f.", finalTotal);      
+
+        // Provides the bill to the customer.
+        System.out.println("\n\n   Here's your bill!");
+        System.out.println("   =================\n");
+        System.out.println(customer1);
+
+    }
+}
---
+++ CafeCustomer.java
@@ -0,0 +1,181 @@
+// Name: Saugat Adhikari
+// Course: CSCI 2003 Spring 2024
+// Program Description: This program creates the customer class and adds the required constructors
+//                      and adds setters and getters for the program, and also adds other methods
+//                      to check the membership, calculate the total costs, and show the bill.
+
+/**
+ * CafeCustomer class represents a customer in a cafe.
+ */
+public class CafeCustomer {
+    /** Name of the customer */
+    private String customerName; 
+    /** Number of coffees ordered by the customer. */
+    private int numOfCoffee;
+    /** Indicates if the customer is a member or not. */ 
+    private boolean isMember;
+    /** Type of coffee ordered by the customer. */    
+    private String coffeeType; 
+    /** Tip given by the customer. */    
+    private double tip;
+    /** Tax rate applied to the total bill. */   
+    private final double TAX = 0.11;  
+
+    /**
+     * Constructs a CafeCustomer object with default values.
+     */
+    public CafeCustomer() {
+        this.customerName = "Customer";
+        this.isMember = false;
+        this.coffeeType = "None";
+        this.numOfCoffee = 0;
+    }
+
+    /**
+     * Constructs a CafeCustomer object with specified parameters.
+     *
+     * @param customerName The name of the customer.
+     * @param numOfCoffee The number of coffees ordered by the customer.
+     * @param isMember Indicates whether the customer is a member or not.
+     * @param coffeeType The type of coffee ordered by the customer.
+     */
+    public CafeCustomer(String customerName, int numOfCoffee, boolean isMember, String coffeeType) {
+        this.customerName = customerName;
+        this.isMember = isMember;
+        this.coffeeType = coffeeType;
+        this.numOfCoffee = numOfCoffee;
+    }
+    /**
+     * Gets the name of the customer.
+     *
+     * @return The name of the customer.
+     */
+    public String getCustomerName(){
+        return this.customerName;
+    }
+
+    /**
+     * Sets the name of the customer.
+     *
+     * @param customerName The name of the customer.
+     */
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+
+    /**
+     * Gets the type of coffee ordered by the customer.
+     *
+     * @return The type of coffee ordered by the customer.
+     */
+    public String getCoffeeType(){
+        return this.coffeeType;
+    }
+
+    /**
+     * Sets the type of coffee ordered by the customer.
+     *
+     * @param coffeeType The type of coffee ordered by the customer.
+     */
+    public void setCoffeeType(String coffeeType){
+        this.coffeeType = coffeeType;
+    }
+
+    /**
+     * Sets the tip given by the customer.
+     *
+     * @param tip The tip given by the customer.
+     */
+    public void setTip(double tip){
+        this.tip = tip;
+    }
+
+    /**
+     * Gets the membership status of the customer.
+     *
+     * @return True if the customer is a member, false otherwise.
+     */
+    public boolean getMembership(){
+        return this.isMember;
+    }
+
+    /**
+     * Sets the membership status of the customer.
+     *
+     * @param isMember True if the customer is a member, false otherwise.
+     */
+    public void setMembership(boolean isMember){
+        this.isMember = isMember;
+    }
+
+
+        /**
+     * Checks if the customer is a member.
+     *
+     * @return True if the customer is a member, false otherwise.
+     */
+    public boolean checkMembership(){
+        if (this.isMember == true){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    /**
+     * Calculates the price of the customer's coffee based on its type.
+     *
+     * @return The price of the customer's coffee.
+     */
+    public double customersCoffeePrice(){
+        double coffeePrice = 0;
+        if (this.coffeeType.equals("cappuccino")){
+            coffeePrice = 4.99;
+        } else if (this.coffeeType.equals("espresso")){
+            coffeePrice = 3.99;
+        } else if (this.coffeeType.equals("latte")){
+            coffeePrice = 6.99;
+        }
+
+        return coffeePrice;
+    }
+    
+    /**
+     * Calculates the subtotal of the customer's bill.
+     *
+     * @return The subtotal of the customer's bill.
+     */
+    public double calcSubTotal(){
+        return (customersCoffeePrice() * this.numOfCoffee);
+    }
+
+    /**
+     * Calculates the total bill amount including tax and discounts.
+     *
+     * @return The total bill amount.
+     */
+    public double calcTotal(){      
+        double discountedTotal = 0.8 * calcSubTotal();
+        if (checkMembership() == true){
+            return (discountedTotal + (discountedTotal * TAX));
+        }
+        else{
+            return (calcSubTotal() + (calcSubTotal() * TAX));
+        }
+    }
+
+    /**
+     * Generates a formatted string representation of the customer's bill.
+     *
+     * @return A formatted string representing the customer's bill.
+     */
+    public String toString(){
+        if(checkMembership() == true){
+            String customerBill = String.format("\t Cafe Bill\n\nFor %s\n\n%d %s\t$%.2f\n\nSub-Total\t\t$%.2f\nDiscount\t\t\t$%.2f\nTip\t\t\t\t$%.2f\nTax 11%%\t\t\t$%.2f\nTotal\t\t\t\t$%.2f",this.customerName, this.numOfCoffee, this.coffeeType, customersCoffeePrice() * this.numOfCoffee, calcSubTotal(), 0.2 * calcSubTotal(), this.tip, calcSubTotal() * this.TAX, calcTotal() + this.tip);
+            return customerBill;
+        }
+        String customerBill = String.format("\t Cafe Bill\n\nFor %s\n\n%d %s\t$%.2f\n\nSub-Total\t\t$%.2f\nDiscount\t\t\t$%.2f\nTip\t\t\t\t$%.2f\nTax 11%%\t\t\t$%.2f\nTotal\t\t\t\t$%.2f",this.customerName, this.numOfCoffee, this.coffeeType, customersCoffeePrice() * this.numOfCoffee, calcSubTotal(), 0.0 * calcSubTotal(), this.tip, calcSubTotal() * this.TAX, calcTotal() + this.tip);
+        return customerBill;        
+    }    
+
+}
